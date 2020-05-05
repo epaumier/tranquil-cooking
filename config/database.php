36db778cef742947fb926d6ae643cd0ba151da95
Tedd <tedd@Edouards-Air.broadband>
2020-05-05 14:47:52 +0100

    yet another syntax error

--- config/database.php
+++ config/database.php
@@ -44,14 +44,6 @@ return [
     |
     */
 
-    $url = parse_url(getenv("DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
-    
-
     'connections' => [
 
         'sqlite' => [
